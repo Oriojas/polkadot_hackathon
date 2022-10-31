85d0d2986e2a4ca22a79f6906e98df612a74dd70
Oriojas <oscarriojas@gmail.com>
2022-10-30 20:57:48 -0500

    feat: plot.pt results

--- plot.py
+++ plot.py
@@ -1,52 +1,36 @@
+import os
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import datetime
 import pyodbc
 
-server = 'esp8266.cnwuu2sdaqvk.us-east-1.rds.amazonaws.com'
-database = 'esp8266'
-username = 'admin'
-password = 'Esp_8266*'
-driver= '{ODBC Driver 17 for SQL Server}'
+SERVER = os.environ["SERVER"]
+DATABASE = os.environ["DATABASE"]
+USERNAME = os.environ["USERNAME"]
+PASSWORD = os.environ["PASSWORD"]
+DRIVER = os.environ["DRIVER"]
 
 # consulta a la base de datos
-with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-    sql_query = f'SELECT * FROM dbo.registro_h_t'
+with pyodbc.connect('DRIVER=' + DRIVER +';SERVER=tcp:' + SERVER + ';PORT=1433;DATABASE=' + DATABASE +';UID=' + USERNAME + ';PWD=' + PASSWORD) as conn:
+    sql_query = f'SELECT * FROM polkadothack.dbo.registro_co2'
     DF = pd.read_sql(sql_query, conn)
 
-DF['FECHA'] = pd.to_datetime(DF['FECHA'])
-end = max(DF['FECHA'])
+DF['DATE_C'] = pd.to_datetime(DF['DATE_C'])
+end = max(DF['DATE_C'])
 init = end - datetime.timedelta(hours=1)
 print(init)
 
-DF = DF[DF['FECHA'] > init]
+DF = DF[DF['DATE_C'] > init]
 
-fig = make_subplots(2, 1)
-fig.add_trace(go.Scatter(x=DF['FECHA'],
-                         y=DF['HUMEDAD'][DF['ORIGEN'] == 'Sensor1'],
-                         name='Humedad (Sensor1)',
+fig = make_subplots(1, 1)
+fig.add_trace(go.Scatter(x=DF['DATE_C'],
+                         y=DF['CO2'][DF['ORIGIN'] == 'Sensor'],
+                         name='CO2 (Sensor)',
                          mode='lines+markers'), 1, 1)
-fig.add_trace(go.Scatter(x=DF['FECHA'],
-                         y=DF['TEMPERATURA'][DF['ORIGEN'] == 'Sensor1'],
-                         name='Temperatura (Sensor1)',
-                         mode='lines+markers',
-                         marker=dict(size=8,
-                                     symbol='hourglass')), 1, 1)
-
-fig.add_trace(go.Scatter(x=DF['FECHA'],
-                         y=DF['HUMEDAD'][DF['ORIGEN'] == 'Sensor2'],
-                         name='Humedad (Sensor2)',
-                         mode='lines+markers'), 2, 1)
-fig.add_trace(go.Scatter(x=DF['FECHA'],
-                         y=DF['TEMPERATURA'][DF['ORIGEN'] == 'Sensor2'],
-                         name='Temperatura (Sensor2)',
-                         mode='lines+markers',
-                         marker=dict(size=8,
-                                     symbol='hourglass')), 2, 1)
 
 template = 'plotly_white'
-fig.update_layout(template=template, title="Temperatura C, Humedad %")
+fig.update_layout(template=template, title="PPM CO2")
 #fig.show()
 
 # convert it to JSON
@@ -63,7 +47,7 @@ template = """<html>
     <br>
     <div class="container px-4">
         <div class="mb-3 border rounded" style='padding: 16px;'>
-            <h2>Datos sensores, una hora atrás</h2>
+            <h2>Datos sensor, una hora</h2>
         </div>
         <div class="mb-3 border rounded" style='padding: 16px;'>
             <div id='divPlotly'></div>
